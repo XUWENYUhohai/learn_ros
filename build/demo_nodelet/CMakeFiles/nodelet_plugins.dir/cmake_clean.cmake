efbe834b673ee7766ee534bbbdb37680f956356a
XUWENYUhohai <xuwenyu982027857@163.com>
2024-10-24 13:41:42 +0800

    nodelet

---
+++ build/demo_nodelet/CMakeFiles/nodelet_plugins.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nodelet_plugins.dir/src/nodelet_plugins.cpp.o"
+  "/home/xuwenyu/learn_ros_ws/devel/lib/libnodelet_plugins.pdb"
+  "/home/xuwenyu/learn_ros_ws/devel/lib/libnodelet_plugins.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nodelet_plugins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/demo_pluginlib/src/polygon_loader.cpp
+++ src/demo_pluginlib/src/polygon_loader.cpp
@@ -1,12 +1,15 @@
+//类加载器相关的头文件
 #include <pluginlib/class_loader.h>
 #include "demo_pluginlib/polygon_base.h"
 
 int main(int argc, char  *argv[])
 {
+    //类加载器 -- 参数1:基类功能包名称 参数2:基类全限定名称
     pluginlib::ClassLoader<polygon_base::RegularPolygon> poly_loader("demo_pluginlib","polygon_base::RegularPolygon");
 
     try
     {
+        //创建插件类实例 -- 参数:插件类全限定名称
         boost::shared_ptr<polygon_base::RegularPolygon> triangle = poly_loader.createInstance("polygon_plugins::Triangle");
         triangle->initialize(10.0);
 
